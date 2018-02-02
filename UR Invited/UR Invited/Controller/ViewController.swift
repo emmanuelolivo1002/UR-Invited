f0f9abe49244f6ffa11601cef7b89e327a7d4461
Emmanuel Olivo <emmanuel@mmacbook-con-dos-emes.local>
2018-02-02 12:51:28 -0600

    Initial Commits

---
+++ UR Invited/UR Invited/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UR Invited
+//
+//  Created by Emmanuel Olivo on 2/02/18.
+//  Copyright © 2018 MJ Invited LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
