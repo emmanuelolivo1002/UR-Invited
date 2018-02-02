d09a9a997201a3338939cc98c66e31dc193a6306
Emmanuel Olivo <emmanuel@mmacbook-con-dos-emes.local>
2018-02-02 16:42:58 -0600

    Add ViewControllers and Fonts

---
+++ UR Invited/UR Invited/Controller/GroupViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GroupViewController.swift
+//  UR Invited
+//
+//  Created by Emmanuel Olivo on 2/02/18.
+//  Copyright © 2018 MJ Invited LLC. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Groups")
+    }
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
