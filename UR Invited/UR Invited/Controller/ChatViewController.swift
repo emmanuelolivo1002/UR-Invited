14e1cf8802b04e6670031753381802c6ca6d189c
Emmanuel Olivo <emmanuel@mmacbook-con-dos-emes.local>
2018-02-06 09:23:33 -0600

    Add keyboard bind feature to ChatViewController and set up delegates

--- UR Invited/UR Invited/Controller/ChatViewController.swift
+++ UR Invited/UR Invited/Controller/ChatViewController.swift
@@ -8,28 +8,133 @@
 
 import UIKit
 
-class ChatViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+   
     
 
-    /*
-    // MARK: - Navigation
+    // MARK: Outlets
+    @IBOutlet weak var chatTableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var composeViewHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var composeView: UIView!
+    @IBOutlet weak var composeViewBottomConstraint: NSLayoutConstraint!
+    
+    // MARK: Variables
+    var keyboardHeight: CGFloat = 0
+    
+    
+    
+    
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    // MARK: Functions
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // TODO: Change placeholder text
+//        var placeHolder = NSMutableAttributedString()
+//
+//        let attributes = [NSAttributedStringKey.font:UIFont(name: "OpenSans-Italic", size: 12.0)! ]
+//
+//        // Set the Font
+//        placeHolder = NSMutableAttributedString(string:"Type a message", attributes: attributes)
+//
+//        //     Add attribute
+//
+//
+//        messageTextField.attributedPlaceholder = placeHolder
+
+        // Set self as Table view delegate and data source
+        chatTableView.delegate = self
+        chatTableView.dataSource = self
+        
+        // Set self as Text Field delegate
+        messageTextField.delegate = self
+        
+        // Tap Gesture
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        chatTableView.addGestureRecognizer(tapGesture)
+        
+        
+
+        // Notifications for keyboard interaction
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+
+
     }
-    */
+    
+    // MARK: Delegate Methods
+    
+    // Table view Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = chatTableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
+        
+        return cell
+    }
+    
+    // tableViewTapped Method
+    @objc func tableViewTapped() {
+        view.endEditing(true)
+    }
 
+    // Textfield Methods
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.25) {
+            self.composeViewHeightConstraint.constant = self.keyboardHeight + 44
+            self.view.layoutIfNeeded()
+        }
+
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.25) {
+
+            self.composeViewHeightConstraint.constant = 44
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+             if self.keyboardHeight >= keyboardSize.height {
+               
+                    self.composeViewHeightConstraint.constant = self.keyboardHeight + 44
+                    self.view.layoutIfNeeded()
+                    // TODO: Scroll to table to bottom
+                
+            } else {
+                self.keyboardHeight = keyboardSize.height
+                
+                    self.composeViewHeightConstraint.constant = self.keyboardHeight + 44
+                    self.view.layoutIfNeeded()
+                
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        UIView.animate(withDuration: 0.5) {
+            self.composeViewHeightConstraint.constant =  44
+            self.view.layoutIfNeeded()
+        }
+        
+            
+        
+    }
+    
 }
+
+    
+
